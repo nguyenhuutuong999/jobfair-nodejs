7dcac8421b280badc52dbff69981203c82af3816
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-05-05 21:42:19 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-let port = process.nextTick.PORT || 3000;
+let port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World")
