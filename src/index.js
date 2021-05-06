1bade79d1259e93ca34aca0a35ab7bc765263d6f
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-05-06 20:57:42 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,22 +3,8 @@ const app = express();
 let port = process.env.PORT || 3001;
 const route = require("./routes");
 const cors = require('cors')
-const morgan = require('morgan');
-const methodOverride = require('method-override');
 
-//connect mongo db
-const db = require('./config')
-db.connect()
-//redirect for other port
 app.use(cors());
-
-app.use(morgan('combined'))
-app.use(express.urlencoded({
-    extended: true
-  }));
-
-app.use(express.json())
-app.use(methodOverride('_method'))
 route(app);
 
 app.listen(port, () =>{
